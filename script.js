cdaeb76e9bde662c3aee5a0263cbe464c444501d
damian2hvergara <damian.2h.vergara@gmail.com>
2025-12-27 23:29:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuración de Supabase
 const SUPABASE_URL = 'https://hzmhobnwqqwamdtzspbv.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_KQ_4Lj4eylyXa91aOj8Qgg_rtJnLGDH';
+const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6bWhvYm53cXF3YW1kdHpzcGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4NzQwMzEsImV4cCI6MjA4MjQ1MDAzMX0.JGb8TpU6tbFfSBi2Gs34YzciYGgQu5gUvWtdnHm6F2I';
 
 // Inicializar Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
